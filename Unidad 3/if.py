77a804fb90231fb8c1865c0a929e382946de7c63
Paola-Quiroz <L211050225@pabellon.tecnm.mx>
2021-11-08 08:23:09 -0600

    modificacion 2

---
+++ Unidad 3/if.py
@@ -0,0 +1,8 @@
+cal = int(input('Ingrese su calificación : '))
+
+if cal >= 7:
+    # New block starts here
+    print('Felicidades, Has aprovado')
+    # New block ends here
+else:
+    print("No aprobaste')
